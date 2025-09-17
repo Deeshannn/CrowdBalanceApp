31d90955e76b73026723d3ca1798cc20270670e8
RidmalChamod <e21065@eng.pdn.ac.lk>
2025-09-17 21:46:14 +0530

    minor changes

--- crowd-balance-mobile-app/config.js
+++ crowd-balance-mobile-app/config.js
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = "http://192.168.1.2:4000"; // Home wifi IP Address
-export const API_BASE_URL = "http://10.108.4.14:4000"; // My phone hotspot IP Address
-// export const API_BASE_URL = "https://crowd-balance-mobile-app-production-7c3f.up.railway.app"; // My phone hotspot IP Address
+// export const API_BASE_URL = "http://10.108.4.14:4000"; // My phone hotspot IP Address
+export const API_BASE_URL = "https://crowd-balance-mobile-app-production-7c3f.up.railway.app"; // My phone hotspot IP Address
 
-export const API_BASE_URL = "http://10.30.251.223:4000"; // My phone hotspot IP Address
+// export const API_BASE_URL = "http://10.30.251.223:4000"; // My phone hotspot IP Address
